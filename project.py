671fd5ca1b30358f81de3897516d3795b0221a9a
Hassan Naveed <hassannaveed.naveed@richmond.edu>
2020-05-01 00:07:12 +0500

    Add files via upload

--- project.py
+++ project.py
@@ -1,6 +1,12 @@
 from tkinter import *
+import pickle
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer
 
-#All the information to train the model will be inserted here.
+#reads in the vectorized features and the logitic regression model
+(cv,lr)  = pickle.load(open('LR_model.pickle', 'rb')) 
+
+#All the information to train the model can be inserted here.
 def trainModel():
 
 	return
@@ -9,12 +15,26 @@ def trainModel():
 #Takes in the word and an integer reprenting which model to use.
 #Returns the prediction of 1 (positive) or -1 (negative) depending on the sentiment
 def analyzeSentiment(word,model):
+	print('sentiment analyszed on \"' + word + "\" and model is: " + model)
+	pos_prob = 0 #probability of being positive
+
 	if model == 'Logistic':
-		pass
+
+		print('opened logistic regression')
+		input_text = [word] #for some reason count vectorization requires a list
+		input_text_vect = cv.transform(input_text)
+		pos_prob = lr.predict_proba(input_text_vect)[0][1]
+		print('probability of positive is: ' + str(pos_prob))
+
+
 	elif model == 'SVM':
 		pass
 
-	print('sentiment analyszed on \"' + word + "\" and model is: " + model)
+	if pos_prob > 0.5:
+		message.configure(background = 'sky blue')
+	else:
+		message.configure(background='indian red')
+	
 	return
 
 #when the enter button is pressed in the text
