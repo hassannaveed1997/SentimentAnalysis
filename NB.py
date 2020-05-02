7b20e4808aaf8434d32ae9cdaf211ad2e6d936dd
mahad.bhatti <43747383+mahadafzal@users.noreply.github.com>
2020-05-02 16:51:47 -0400

    cleaned up code for nb and svm

--- NB.py
+++ NB.py
@@ -1,54 +1,55 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import accuracy_score
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
-import pickle
-import csv
-import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
 import os
-import re
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
 
-#read in the dataset of cleaned tweets
+# Multinomial Naive Bayes Classifier
+# This file implements a MultinomiaNB Classifier and reports results based
+# on the processed Tweet dataset.
+
+# Importing csv file into a dataframe
 data = pd.read_csv('cleaned_data.csv')
+data.columns = ['Number','Sentiment','Tweet']
 
-#randomly set 80% of the data for training and 20% for testing
-train=data.sample(frac=0.8,random_state=50) 
+# Train-test split with seed value 50
+train=data.sample(frac=0.8,random_state=50)
 test=data.drop(train.index)
 
-#seperate the Tweet and Sentiment columns
-train_text = train['Tweet']
-train_values = train['Sentiment']
-test_text = test['Tweet']
-test_values = test['Sentiment']
+# Assigning data to respective lists
+Train_text = train['Tweet']
+Train_values = train['Sentiment']
+Test_text = test['Tweet']
+Test_values = test['Sentiment']
 
-#A count vectorizer creates a huge dataframe with each word in the corpus.
-#Setting binary to True has the dataframe contain one or zero depending on whether or not a word appears in a tweet.
-#Alternative(binary = False) or use TF-IDF vectorizor
-cv = CountVectorizer(binary=True)
+# CV and TF-IDF Vectorizers (Uncomment code block to use)
 
-#fit the model on the existing training corpus (Note: text needs to encoded as unicode, so astype('U') is included)
-cv.fit(train_text.values.astype('U'))
+# vectorizer = CountVectorizer(binary=True, ngram_range= (1,1))
+# vectorizer.fit(Train_text.values.astype('U'))
+# Train_text_vect = vectorizer.transform(Train_text.values.astype('U'))
+# Test_text_vect = vectorizer.transform(Test_text.values.astype('U'))
 
-#creates the dataframes on the training and test data
-train_text_vect = cv.transform(train_text.values.astype('U'))
-test_text_vect = cv.transform(test_text.values.astype('U'))
+vectorizer = TfidfVectorizer(ngram_range = (1,3),min_df = 2)
+vectorizer.fit(Train_text.values.astype('U'))
+Train_text_vect = vectorizer.transform(Train_text.values.astype('U'))
+Test_text_vect = vectorizer.transform(Test_text.values.astype('U'))
 
+#MultinomialNB that outputs classification report
+nb = MultinomialNB()
+nb.fit(Train_text_vect, Train_values)
+score = accuracy_score(Test_values, nb.predict(Test_text_vect))
 
-a = 0.5 #hyperparameter value for the Mutlinomial Naive Bayes
+print(score)
 
-#fit and train the Multinomial Naive Bayes
-nb = MultinomialNB(a)
-nb.fit(train_text_vect, train_values)
+# Create a dataframe containing coefficients from the regression 
+# and features from the count vectorizor for analysis.
 
-#predict and score
-score = accuracy_score(test_values, nb.predict(test_text_vect))
-print ("Accuracy Score for A ="+"  str(a) "+" equals:   " + str(score))
-#for uncleaned data this is:
-
-#Now we will extract the features for analysis
-#create a dataframe containing coefficients from the regression and features from the count vectorizor
-features = pd.DataFrame(cv.get_feature_names(), nb.coef_.tolist()).reset_index()
+features = pd.DataFrame(vectorizer.get_feature_names(), nb.coef_.tolist()).reset_index()
 
 #rename the columns
 features.columns = ['coef','feature']
@@ -56,14 +57,13 @@ features.columns = ['coef','feature']
 #sort the features (most negative first)
 features = features.sort_values(by=['coef'])
 
-#print the 5 most positive and negative features
-print(features.head(5))
-print(features.tail(5))
+# Print the 5 most positive and negative features
+#print(features.head(5))
+#print(features.tail(5))
 
+
+# Save vectorizer and nb model to a pickle file in the models folder
 if not os.path.exists("pickled_models"):
-    os.makedirs("pickled_models")
-    
-save_nb = open("pickled_models/nb_model.pickle", "wb")
-pickle.dump(nb, save_nb)
-save_svm.close()
+	os.makedirs("pickled_models")
 
+pickle.dump((vectorizer,nb), open('pickled_models/nb_model.pickle', 'wb'))
--- svm.py
+++ svm.py
@@ -1,48 +1,57 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
 from sklearn.model_selection import train_test_split
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
 import os
+from sklearn.metrics import classification_report
 
-#importing data
+# SVC Classifier
+# This file implements a LinearSVC Classifier and reports results based
+# on the processed Tweet dataset.
+
+# Importing csv file into a dataframe
 data = pd.read_csv('cleaned_data.csv')
-data.columns = ['Sentiment','Tweet']
+data.columns = ['Number','Sentiment','Tweet']
 
-train=data.sample(frac=0.8,random_state=50) #random state is a seed value
+# Train-test split with seed value 50
+train=data.sample(frac=0.8,random_state=50)
 test=data.drop(train.index)
 
-# assigning data to respective lists
+# Assigning data to respective lists
 Train_text = train['Tweet']
 Train_values = train['Sentiment']
 Test_text = test['Tweet']
 Test_values = test['Sentiment']
 
-#CV with ngram range 1,2
-cv = CountVectorizer(binary=True, ngram_range= (1,2))
-cv.fit(Train_text.values.astype('U'))
-Train_text_vect = cv.transform(Train_text.values.astype('U'))
-Test_text_vect = cv.transform(Test_text.values.astype('U'))
+# CV and TF-IDF Vectorizers (Uncomment code block to use)
 
-#TF-IDF with min doc frequency 2
-# tfidf = TfidfVectorizer(ngram_range = (1,3),min_df = 2)
-# tfidf.fit(Train_text.values.astype('U'))
-# Train_text_vect = tfidf.transform(Train_text.values.astype('U'))
-# Test_text_vect = tfidf.transform(Test_text.values.astype('U'))
+# vectorizer = CountVectorizer(binary=True, ngram_range= (1,1))
+# vectorizer.fit(Train_text.values.astype('U'))
+# Train_text_vect = vectorizer.transform(Train_text.values.astype('U'))
+# Test_text_vect = vectorizer.transform(Test_text.values.astype('U'))
 
-#SVM model
-svm = LinearSVC(C=0.01)
+vectorizer = TfidfVectorizer(ngram_range = (1,3),min_df = 2)
+vectorizer.fit(Train_text.values.astype('U'))
+Train_text_vect = vectorizer.transform(Train_text.values.astype('U'))
+Test_text_vect = vectorizer.transform(Test_text.values.astype('U'))
+
+#Linear SVC model that outputs classification report (change c value to investigate regularization parameter)
+c = 0.25
+svm = LinearSVC(C=c)
 svm.fit(Train_text_vect, Train_values)
-score = accuracy_score(Test_values, svm.predict(Test_text_vect))
+score = classification_report(Test_values, svm.predict(Test_text_vect))
 
+print("For c value", + c)
 print(score)
 
+# Create a dataframe containing coefficients from the regression 
+# and features from the count vectorizor for analysis.
 
-#Now we will extract the features for analysis
-#create a dataframe containing coefficients from the regression and features from the count vectorizor
-features = pd.DataFrame(cv.get_feature_names(), svm.coef_.tolist()).reset_index()
+features = pd.DataFrame(vectorizer.get_feature_names(), svm.coef_.tolist()).reset_index()
 
 #rename the columns
 features.columns = ['coef','feature']
@@ -50,13 +59,13 @@ features.columns = ['coef','feature']
 #sort the features (most negative first)
 features = features.sort_values(by=['coef'])
 
-#print the 5 most positive and negative features
-print(features.head(5))
-print(features.tail(5))
+# Print the 5 most positive and negative features
+#print(features.head(5))
+#print(features.tail(5))
 
 
-# Save SVM model
+# Save vectorizer and SVM model to a pickle file in the models folder
 if not os.path.exists("pickled_models"):
 	os.makedirs("pickled_models")
-	
-pickle.dump((cv,svm), open('pickled_models/svm_model.pickle', 'wb'))
+
+pickle.dump((vectorizer,svm), open('pickled_models/svm_model.pickle', 'wb'))
