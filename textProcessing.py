5208476e65648773de19c0271d0777953c96d35e
Abdul Qadeer Rehan <47748553+abdulqadeer47@users.noreply.github.com>
2020-05-02 01:23:10 -0400

    Self implemented stop words functions added

--- textProcessing.py
+++ textProcessing.py
@@ -2,10 +2,64 @@
 ###################### USING NLTK LIBRARY #############################
 #######################################################################
 from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 import pandas as pd
 
+def processText(text):
+
+    # make a list of words split with spaces
+    textList = text.split()
+
+    #convert all words to lower case
+    for i in range(len(textList)):
+        textList[i] = textList[i].lower()
+
+    #removes Stop words
+    for t in textList:
+        if (removeStopWords(t)):
+            textList.remove(t)
+    text = ' '.join(textList)
+
+    #tokenize by words
+    words = word_tokenize(text)
+
+    #instance of a stemmer
+    stemmer = SnowballStemmer("english")
+    stemWords = []
+
+
+    #iterate over all tokenized words to stem and store in list
+    for t in words:
+        stemWords.append(stemmer.stem(t))
+
+        #return the list of tokenized stem words of the text passed to the method
+    return stemWords
+
+
+# This method takes a single string word checks it against
+# a list of substrings to determine whether the word has 
+# any useful information regarding the sentiment of the text.
+# returns True if word is an email address, username, hashtag or 
+# website else returns False.
+def removeStopWords(word):
+
+    # List of substrings to check if present in the word:
+    # '@',".edu", ".com" may represent usernames and email addresses 
+    # '#' represents hastags for social media 
+    # 'www.', "https://", ".edu", ".com" may represent websites
+    # increased the list by adding stop words from the nltk stop words function
+    # The nltk stopwords had negations such as not, nor, won't which are not
+    # considered stopwords in this method
+    cleaners = ["@", "#", "www.", "Www.", "https://", "Https://" ,  ".edu", ".com", 'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such','only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just','ll', 'm', 'o', 're', 've', 'y', 'ma']
+
+    #check each subtring if contained in word
+    for c in cleaners:
+        #if word contains substring return True
+        if c in word:
+            return True
+
+    #else return False
+    return False
 
 #read data from .csv file
 dataset = pd.read_csv(r'training.1600000.processed.noemoticon.csv',encoding = "ISO-8859-1")
@@ -55,64 +109,6 @@ for j in range(len(neutral)):
 
 #Create a list for training (need to add corresponding labelList)
 trainList = posList + negList + neutralList
-
-def processText(text):
-
-    # make a list of words split with spaces
-    textList = text.split()
-
-    #convert all words to lower case
-    for i in range(len(textList)):
-        textList[i] = textList[i].lower()
-
-    #remove usernames, email addresses, websites and hastags
-    for t in textList:
-        if (deepClean(t)):
-            textList.remove(t)
-    text = ' '.join(textList)
-
-	#tokenize by words
-    words = word_tokenize(text)
-
-    #For each word in the list check if it is a stop word
-    for w in words:
-        if w in stopwords.words('english'):
-            words.remove(w)
-
-    #instance of a stemmer
-    stemmer = SnowballStemmer("english")
-    stemWords = []
-
-
-    #iterate over all tokenized words to stem and store in list
-    for t in words:
-    	stemWords.append(stemmer.stem(t))
-
-    	#return the list of tokenized stem words of the text passed to the method
-    return stemWords
-
-
-# This method takes a single string word checks it against
-# a list of substrings to determine whether the word has 
-# any useful information regarding the sentiment of the text.
-# returns True if word is an email address, username, hashtag or 
-# website else returns False.
-def deepClean(word):
-
-    # List of substrings to check if present in the word:
-    # '@',".edu", ".com" may represent usernames and email addresses 
-    # '#' represents hastags for social media 
-    # 'www.', "https://", ".edu", ".com" may represent websites
-    cleaners = ["@", "#", "www.", "Www.", "https://", "Https://" ,  ".edu", ".com"]
-
-    #check each subtring if contained in word
-    for c in cleaners:
-        #if word contains substring return True
-        if c in word:
-            return True
-
-    #else return False
-    return False
 
 
 #Hassan:
