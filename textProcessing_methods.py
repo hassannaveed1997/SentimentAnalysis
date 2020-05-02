4ae3a8bc092c54c526d306ade8d0c0ea342d7288
Abdul Qadeer Rehan <47748553+abdulqadeer47@users.noreply.github.com>
2020-05-02 17:41:05 -0400

    Separated methods into separate file

---
+++ textProcessing_methods.py
@@ -0,0 +1,86 @@
+from nltk.tokenize import word_tokenize
+from nltk.stem.snowball import *
+#from nltk.stem.porter import *
+#from nltk.stem import WordNetLemmatizer
+#from nltk.corpus import stopwords
+
+# processText takes in a string as it's parameter 
+# removes stopwords such as usernames, email addresses,
+# websites and hashtags and returns a tokenized and 
+# stemmed list of words from the text.
+def processText(text):
+
+    # make a list of words split with spaces
+    textList = text.split()
+
+    #convert all words to lower case
+    for i in range(len(textList)):
+        textList[i] = textList[i].lower()
+
+    #remove usernames, email addresses, websites and hastags
+    for t in textList:
+        if (deepClean(t)):
+            textList.remove(t)
+    text = ' '.join(textList)
+
+    #tokenize by words
+    words = word_tokenize(text)
+
+    #For each word in the list check if it is a stop word using nltk function
+    #resulted in depreciated performace so not being used
+    #for w in words:
+    #    if w in stopwords.words('english'):
+    #        words.remove(w)
+
+
+    #instance of a Snowball stemmer
+    stemmer = SnowballStemmer("english")
+    
+    # instance of a Porter stemmer
+    # was tested. performed worse than Porter stemmer
+    #stemmer = PorterStemmer()
+
+    stemWords = []
+
+    #iterate over all tokenized words to stem and store in list
+    for t in words:
+        stemWords.append(stemmer.stem(t))
+
+    #return the list of tokenized stem words of the text passed to the method
+    return stemWords
+
+
+    # Lemmatize words and store in list
+    # Lemmatization was tested, performed worse than stemming
+    # lemmatizer = WordNetLemmatizer()
+    # lemWords = []
+    # for w in words:
+    #    lemWords.append(lemmatizer.lemmatize(w))
+    # lemmatizer = WordNetLemmatizer()
+    # lemWords = []
+    # for w in words:
+    #     lemWords.append(lemmatizer.lemmatize(w))
+    #return lemWords
+
+
+# This method takes a single string word checks it against
+# a list of substrings to determine whether the word has 
+# any useful information regarding the sentiment of the text.
+# returns True if word is an email address, username, hashtag or 
+# website else returns False.
+def deepClean(word):
+
+    # List of substrings to check if present in the word:
+    # '@',".edu", ".com" may represent usernames and email addresses 
+    # '#' represents hastags for social media 
+    # 'www.', "https://", ".edu", ".com" may represent websites
+    cleaners = ["@", "#", "www.", "Www.", "https://", "Https://" ,"http" ,".edu", ".com"]
+
+    #check each subtring if contained in word
+    for c in cleaners:
+        #if word contains substring return True
+        if c in word:
+            return True
+
+    #else return False
+    return False
